59509ba4f5a24f5a5413c432b933075b4800b1d5
Florencia Rímolo Figueira <florencia.rimolo@fib.upc.edu>
2017-06-08 10:54:56 +0200

    mostra icona FIB

--- app/src/main/java/com/inlab/racodemoapi/Activities/MainMenuActivity.java
+++ app/src/main/java/com/inlab/racodemoapi/Activities/MainMenuActivity.java
@@ -2,8 +2,13 @@ package com.inlab.racodemoapi.Activities;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
@@ -11,6 +16,8 @@ import com.inlab.racodemoapi.Models.User;
 import com.inlab.racodemoapi.R;
 import com.inlab.racodemoapi.ServiceSettings.RacoAPIService;
 import com.inlab.racodemoapi.ServiceSettings.ServiceGenerator;
+
+import java.io.InputStream;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -25,19 +32,29 @@ public class MainMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
         prefs = this.getSharedPreferences("com.inlab.racodemoapi", Context.MODE_PRIVATE);
-        accessToken  = prefs.getString("accessToken", null);
+        accessToken = prefs.getString("accessToken", null);
         System.out.println(accessToken);
-        RacoAPIService racoAPIService = ServiceGenerator.createService(RacoAPIService.class,LoginActivity.clientId,LoginActivity.clientSecret,accessToken);
+        RacoAPIService racoAPIService = ServiceGenerator.createService(RacoAPIService.class, LoginActivity.clientId, LoginActivity.clientSecret, accessToken);
         Call<User> call1 = racoAPIService.getMyInfo();
-        final TextView textView = (TextView) findViewById(R.id.textView);
+        final TextView textViewJo = (TextView) findViewById(R.id.textViewJo);
+        final TextView textViewUsername = (TextView) findViewById(R.id.textViewUsername);
+        final TextView textViewEmail = (TextView) findViewById(R.id.textViewEmail);
         call1.enqueue(new Callback<User>() {
             @Override
             public void onResponse(Call<User> call, Response<User> response) {
                 String json = new Gson().toJson(response.body());
                 System.out.println(json);
                 String jo = response.body().getNom();
-                String welcome = "Welcome "+jo;
-                textView.setText(welcome);
+                jo += " " + response.body().getCognoms();
+                String username = response.body().getUsername();
+                String email = response.body().getEmail();
+                String image = response.body().getFoto();
+                new DownloadImageFromInternet((ImageView) findViewById(R.id.imageView))
+                        .execute(image);
+
+                textViewJo.setText(jo);
+                textViewUsername.setText(username);
+                textViewEmail.setText(email);
             }
 
             @Override
@@ -46,4 +63,31 @@ public class MainMenuActivity extends AppCompatActivity {
             }
         });
     }
+
+    private class DownloadImageFromInternet extends AsyncTask<String, Void, Bitmap> {
+        ImageView imageView;
+
+        public DownloadImageFromInternet(ImageView imageView) {
+            this.imageView = imageView;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String imageURL = urls[0];
+            Bitmap bimage = null;
+            try {
+                InputStream in = new java.net.URL(imageURL).openStream();
+                bimage = BitmapFactory.decodeStream(in);
+
+            } catch (Exception e) {
+                Log.e("Error Message", e.getMessage());
+                e.printStackTrace();
+            }
+            return bimage;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            imageView.setImageBitmap(result);
+        }
+    }
 }
+
