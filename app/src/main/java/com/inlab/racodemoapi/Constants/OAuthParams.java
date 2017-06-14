dd7ec62ce12db4f347d50155e92463bfb902d463
Florencia Rímolo Figueira <florencia.rimolo@fib.upc.edu>
2017-06-14 10:59:22 +0200

    afegit comentari

--- app/src/main/java/com/inlab/racodemoapi/Constants/OAuthParams.java
+++ app/src/main/java/com/inlab/racodemoapi/Constants/OAuthParams.java
@@ -1,6 +1,9 @@
 package com.inlab.racodemoapi.Constants;
 
 public class OAuthParams {
+    /** Here you have to replace the clientID and the clientSecret variables with your own application
+     * values.
+     * **/
     public static final String clientID = "o0OQumjT6MfVQsry7TFE2WnGD6wtFifxXDBlphei";
     public static final String clientSecret = "Uvs2ukTaoTgLj6j3NSUiMEt3YhdbTqhx5ETCLpkR3hT4JuzqM0y5EBzk0QQhjnj89PpTZtxFEFpn5v3uv5GDUz4UNdgYqf1nIwzYt65KpyaPOsrRJQRyHc54viBmiZLJ";
     public static final String redirectUri = "apifib://raco";
