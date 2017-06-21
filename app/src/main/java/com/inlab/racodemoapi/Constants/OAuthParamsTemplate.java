1b1c92fa5b49bf774910e59fab822dac2d1645f0
Florencia Rímolo Figueira <florencia.rimolo@fib.upc.edu>
2017-06-21 13:31:14 +0200

    afegida plantilla constants

---
+++ app/src/main/java/com/inlab/racodemoapi/Constants/OAuthParamsTemplate.java
@@ -0,0 +1,9 @@
+package com.inlab.racodemoapi.Constants;
+
+
+public class OAuthParamsTemplate {
+    public static final String clientID = "";
+    public static final String clientSecret = "";
+    public static final String redirectUri = "apifib://raco";
+    public static String responseType = "code";
+}
